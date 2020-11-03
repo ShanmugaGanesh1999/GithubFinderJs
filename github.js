3512862a564082489ec541b2e0040afd1b9cf455
Shanmuga Ganesh T <60395810+ShanmugaGanesh1999@users.noreply.github.com>
2020-11-03 16:12:37 +0530

    Add files via upload

---
+++ github.js
@@ -0,0 +1,21 @@
+class Github {
+    constructor() {
+        this.client_id = 'c91799ce11b40f0d8944';
+        this.client_secret = 'bf7f6fe00871af6a962d1691490445d75d7d1c11';
+        this.repos_count = 10;
+        this.repos_sort = 'created: asc';
+    }
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
